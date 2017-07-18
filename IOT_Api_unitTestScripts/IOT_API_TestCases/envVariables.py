dab09a605dc2a3c4fcaf363a36c3ec08b0ea551e
xt <312324191@qq.com>
2017-07-18 23:45:27 +0800

    111

---
+++ IOT_Api_unitTestScripts/IOT_API_TestCases/envVariables.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+__author__ = 'XT'
+
+
+host = 'http://122.97.128.13:8808'
+
+mysqlConnect = ("122.97.128.13",3908,"dbesbcfg","dbesbcfg")
+
+# oracleConnect = ('dbvop', 'dbvop', '10.161.50.88:1530/TDB')
+
