b3f67d4b11d8f134c040307fbfe1ae978b21eef9
XT <3123242191@qq.com>
2017-09-10 23:03:04 +0800

    UI

---
+++ uitest/baidu.py
@@ -0,0 +1,16 @@
+#-*- coding:utf-8 -*-
+from selenium import webdriver
+from time import sleep
+try:
+	driver = webdriver.Chrome()
+	driver.maximize_window()
+	driver.get("http://www.baidu.com")
+	driver.find_element_by_name("wd").send_keys("selenium")
+	driver.find_element_by_id("su").click()
+	sleep(2)
+except Exception as e:
+	raise e
+finally:
+	driver.quit()
+
+
